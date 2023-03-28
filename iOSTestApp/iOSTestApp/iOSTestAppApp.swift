de90886554cfa240462b8fe4865834fc22e5fa04
Purvesh Dodiya <purvesh.d@simformsolutions.com>
2023-03-28 21:53:18 +0530

    iOS Project added

---
+++ iOSTestApp/iOSTestApp/iOSTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSTestAppApp.swift
+//  iOSTestApp
+//
+//  Created by Purvesh Dodiya on 28/03/23.
+//
+
+import SwiftUI
+
+@main
+struct iOSTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
