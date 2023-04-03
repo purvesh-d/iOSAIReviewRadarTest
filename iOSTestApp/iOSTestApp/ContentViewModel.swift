77f44b812ac2b7ff7209763ea1210c43e1ab319c
Purvesh Dodiya <purvesh.d@simformsolutions.com>
2023-04-03 18:23:19 +0530

    Added basic function.

--- iOSTestApp/iOSTestApp/ContentViewModel.swift
+++ iOSTestApp/iOSTestApp/ContentViewModel.swift
@@ -13,8 +13,11 @@ class ContentViewModel: ObservableObject {
     @Published private var firstName = ""
     @Published var lastName = ""
     @Published var email = ""
+    @Published var elephantCount = 20
+    @Published var contactVisible = 10
     @Published var needToShowAlert = false
     @Published var errorText = ""
+    @Published var xv = 10
     
 }
 
@@ -26,6 +29,40 @@ extension ContentViewModel {
             debugPrint("Email is valid")
         } else {
             debugPrint("Email is not valid")
+        }
+    }
+    
+}
+
+extension ContentViewModel {
+    
+    func getContactVisibleCount() -> Int {
+        return contactVisible
+    }
+    
+    func getElephantCount() -> Int{
+        return elephantCount
+    }
+    
+    struct UserData {
+        var firstName: String
+        var lastName: String
+        var email: String
+        var errorText: String
+        var xv: Int
+    }
+    
+    func saveUserData(_ userData: UserData) {
+        debugPrint(firstName)
+        debugPrint(lastName)
+    }
+    
+    func CheckForSwitch(date: String) {
+        switch date {
+        case "Hello":
+            break
+        default:
+            break
         }
     }
     
